027f8afadf9a4a45bc94087b7eba6d9b6bccad3f
LadPaule <ssozipaule@gmail.com>
2021-02-28 09:41:30 +0300

    scaled video

--- src/components/videoItem.js
+++ src/components/videoItem.js
@@ -5,7 +5,7 @@ const VideoItem = ({ video, onVideoSelect }) => {
   return ( 
     <Grid item xs={12}>
       <Paper style={{display: "flex", alignItems: "center", cursor: 'pointer', maxWidth: "100%"}} onClick = {()=>{onVideoSelect(video)}}>
-        <img src={video.snippet.thumbnails.medium.url} alt="thumbnail" style={{marginRight: "10px"}}/>
+        <img src={video.snippet.thumbnails.medium.url} alt="thumbnail" style={{marginRight: "2px", width: "240px", height: "135px"}}/>
         <Typography style ={{textSizeAdjust: "auto"}} variant ="subtitle1"><b>{video.snippet.title}</b></Typography>
       </Paper>
     </Grid>
